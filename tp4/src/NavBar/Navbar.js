cb314f2934a4adb7891b45a1a2909f8efc262f62
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-10-11 11:40:07 +0200

    essai

--- tp4/src/NavBar/Navbar.js
+++ tp4/src/NavBar/Navbar.js
@@ -3,10 +3,9 @@ import React, { useState, useEffect } from 'react'
 export default function Navbar() {
     return (
         <nav>
-            <ul className="liste">
-                <li className="btn"></li>
 
-            </ul>
+
+
         </nav>
     )
 }
