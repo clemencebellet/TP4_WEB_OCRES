85e2b3b276139b15f5fd20a44bb4d8f50cfefcbd
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-10-11 12:34:47 +0200

    Changement anciens fichiers

---
+++ tp4/src/index/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../containers/Navbar";
+import User from "../containers/user";
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [
+
+            ],
+            selectedUsers: 'Jeanne'
+        }
+    }
+    changeUser(selectedUsers)
+}
+
