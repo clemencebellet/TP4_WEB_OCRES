532137b57a9ab6c50932d1206ea885cce1473c18
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-10-11 12:35:11 +0200

    suppression dossiers

--- tp4/src/App.js
+++ tp4/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './containers/Navbar';
+
 
 function App() {
   return (
---
+++ tp4/src/containers/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '.../index.css';
+
+
+export default class Nav extends React.Component {
+    render() {
+        const { users, changeUser } = this.props;
+
+        return (
+            <div class='nav'>
+
+
+            </div>
+        )
+
+
+
+
+    }
+}
+
