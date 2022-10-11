e366f335d2e20929cbc7061dfebdd24b88a2f7de
clemencebellet <78761529+clemencebellet@users.noreply.github.com>
2022-10-11 11:36:40 +0200

    Création tp4 react

---
+++ tp4/src/NavBar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul className="liste">
+                <li className="btn"></li>
+
+            </ul>
+        </nav>
+    )
+}
